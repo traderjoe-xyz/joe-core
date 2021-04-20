14312229b23de543e56fc0a650afd7d4c8122bbd
cryptofish7 <cryptofish7@protonmail.com>
2021-04-19 22:35:15 -0400

    Refactor to PANGOLIN_ROUTER

--- deploy/JoeRoll.js
+++ deploy/JoeRoll.js
@@ -1,13 +1,16 @@
-const JOE_ROUTER = new Map();
-JOE_ROUTER.set("1", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-JOE_ROUTER.set("3", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-JOE_ROUTER.set("4", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-JOE_ROUTER.set("5", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-JOE_ROUTER.set("42", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
-JOE_ROUTER.set("1287", "0x2823caf546C7d09a4832bd1da14f2C6b6E665e05");
-JOE_ROUTER.set("43113", "0x2D99ABD9008Dc933ff5c0CD271B88309593aB921");
-JOE_ROUTER.set("43114", "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106");
-JOE_ROUTER.set("79377087078960", "0x0B72c0193CD598b536210299d358A5b720A262b8");
+const PANGOLIN_ROUTER = new Map();
+PANGOLIN_ROUTER.set("1", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+PANGOLIN_ROUTER.set("3", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+PANGOLIN_ROUTER.set("4", "0xE8A191026e71C303b0f40F15be93403f7D529707");
+PANGOLIN_ROUTER.set("5", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+PANGOLIN_ROUTER.set("42", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+PANGOLIN_ROUTER.set("1287", "0x2823caf546C7d09a4832bd1da14f2C6b6E665e05");
+PANGOLIN_ROUTER.set("43113", "0x2D99ABD9008Dc933ff5c0CD271B88309593aB921");
+PANGOLIN_ROUTER.set("43114", "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106");
+PANGOLIN_ROUTER.set(
+  "79377087078960",
+  "0x0B72c0193CD598b536210299d358A5b720A262b8"
+);
 
 module.exports = async function ({
   getNamedAccounts,
@@ -20,17 +23,17 @@ module.exports = async function ({
 
   const chainId = await getChainId();
 
-  if (!JOE_ROUTER.has(chainId)) {
-    throw Error("No Joe Router");
+  if (!PANGOLIN_ROUTER.has(chainId)) {
+    throw Error("No Pangolin Router");
   }
 
-  const pangolinRouterAddress = JOE_ROUTER.get(chainId);
+  const pangolinRouterAddress = PANGOLIN_ROUTER.get(chainId);
 
-  const joeswapRouterAddress = (await deployments.get("JoeRouter02")).address;
+  const joeRouterAddress = (await deployments.get("JoeRouter02")).address;
 
   await deploy("JoeRoll", {
     from: deployer,
-    args: [pangolinRouterAddress, joeswapRouterAddress],
+    args: [pangolinRouterAddress, joeRouterAddress],
     log: true,
     deterministicDeployment: false,
   });
