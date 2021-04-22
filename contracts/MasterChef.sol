e8b2bf4c17f612b306b527d52b3d99e29184f681
cryptofish7 <cryptofish7@protonmail.com>
2021-04-22 00:26:06 -0400

    Use new MasterChef based on timestamp and with added events

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -29,12 +29,14 @@ interface IMigratorJoe {
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
+// With thanks to the Lydia Finance team.
 contract MasterChef is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
+
     // Info of each user.
     struct UserInfo {
-        uint256 amount; // How many LP tokens the user has provided.
+        uint256 amount;     // How many LP tokens the user has provided.
         uint256 rewardDebt; // Reward debt. See explanation below.
         //
         // We do some fancy math here. Basically, any point in time, the amount of JOEs
@@ -43,58 +45,64 @@ contract MasterChef is Ownable {
         //   pending reward = (user.amount * pool.accJoePerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accJoePerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accJoePerShare` (and `lastRewardTimestamp`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
     }
+
     // Info of each pool.
     struct PoolInfo {
-        IERC20 lpToken; // Address of LP token contract.
-        uint256 allocPoint; // How many allocation points assigned to this pool. JOEs to distribute per block.
-        uint256 lastRewardBlock; // Last block number that JOEs distribution occurs.
+        IERC20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. JOEs to distribute per second.
+        uint256 lastRewardTimestamp;  // Last timestamp that JOEs distribution occurs.
         uint256 accJoePerShare; // Accumulated JOEs per share, times 1e12. See below.
     }
+
     // The JOE TOKEN!
     JoeToken public joe;
     // Dev address.
     address public devaddr;
-    // Block number when bonus JOE period ends.
-    uint256 public bonusEndBlock;
-    // JOE tokens created per block.
-    uint256 public joePerBlock;
+    // Treasury address.
+    address public treasuryaddr;
+    // JOE tokens created per second.
+    uint256 public joePerSec;
     // Bonus muliplier for early joe makers.
-    uint256 public constant BONUS_MULTIPLIER = 10;
-    // The migrator contract. It has a lot of power. Can only be set through governance (owner).
-    IMigratorJoe public migrator;
+    uint256 public BONUS_MULTIPLIER = 1;
+
     // Info of each pool.
     PoolInfo[] public poolInfo;
     // Info of each user that stakes LP tokens.
-    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
-    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when JOE mining starts.
-    uint256 public startBlock;
+    // The timestamp when JOE mining starts.
+    uint256 public startTimestamp;
+
+    event Add(address indexed lpToken, uint256 allocPoint);
+    event Set(uint256 indexed pid, uint256 allocPoint);
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
-    event EmergencyWithdraw(
-        address indexed user,
-        uint256 indexed pid,
-        uint256 amount
-    );
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event SetDevAddress(address indexed oldAddress, address indexed newAddress);
+    event UpdateEmissionRate(address indexed user, uint256 _joePerSec);
 
     constructor(
         JoeToken _joe,
         address _devaddr,
-        uint256 _joePerBlock,
-        uint256 _startBlock,
-        uint256 _bonusEndBlock
+        address _treasuryaddr,
+        uint256 _joePerSec,
+        uint256 _startTimestamp
     ) public {
         joe = _joe;
         devaddr = _devaddr;
-        joePerBlock = _joePerBlock;
-        bonusEndBlock = _bonusEndBlock;
-        startBlock = _startBlock;
+        treasuryaddr = _treasuryaddr;
+        joePerSec = _joePerSec;
+        startTimestamp = _startTimestamp;
+    }
+
+    function updateMultiplier(uint256 multiplierNumber) public onlyOwner {
+        BONUS_MULTIPLIER = multiplierNumber;
     }
 
     function poolLength() external view returns (uint256) {
@@ -103,102 +111,51 @@ contract MasterChef is Ownable {
 
     // Add a new lp to the pool. Can only be called by the owner.
     // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
-    function add(
-        uint256 _allocPoint,
-        IERC20 _lpToken,
-        bool _withUpdate
-    ) public onlyOwner {
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
         if (_withUpdate) {
             massUpdatePools();
         }
-        uint256 lastRewardBlock =
-            block.number > startBlock ? block.number : startBlock;
+        uint256 lastRewardTimestamp = block.timestamp > startTimestamp ? block.timestamp : startTimestamp;
         totalAllocPoint = totalAllocPoint.add(_allocPoint);
-        poolInfo.push(
-            PoolInfo({
-                lpToken: _lpToken,
-                allocPoint: _allocPoint,
-                lastRewardBlock: lastRewardBlock,
-                accJoePerShare: 0
-            })
-        );
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardTimestamp: lastRewardTimestamp,
+            accJoePerShare: 0
+        }));
+        emit Add(address(_lpToken), _allocPoint);
     }
 
     // Update the given pool's JOE allocation point. Can only be called by the owner.
-    function set(
-        uint256 _pid,
-        uint256 _allocPoint,
-        bool _withUpdate
-    ) public onlyOwner {
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
         if (_withUpdate) {
             massUpdatePools();
         }
-        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(
-            _allocPoint
-        );
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
         poolInfo[_pid].allocPoint = _allocPoint;
+        emit Set(_pid, _allocPoint);
     }
 
-    // Set the migrator contract. Can only be called by the owner.
-    function setMigrator(IMigratorJoe _migrator) public onlyOwner {
-        migrator = _migrator;
-    }
-
-    // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
-    function migrate(uint256 _pid) public {
-        require(address(migrator) != address(0), "migrate: no migrator");
-        PoolInfo storage pool = poolInfo[_pid];
-        IERC20 lpToken = pool.lpToken;
-        uint256 bal = lpToken.balanceOf(address(this));
-        lpToken.safeApprove(address(migrator), bal);
-        IERC20 newLpToken = migrator.migrate(lpToken);
-        require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
-        pool.lpToken = newLpToken;
-    }
-
-    // Return reward multiplier over the given _from to _to block.
-    function getMultiplier(uint256 _from, uint256 _to)
-        public
-        view
-        returns (uint256)
-    {
-        if (_to <= bonusEndBlock) {
-            return _to.sub(_from).mul(BONUS_MULTIPLIER);
-        } else if (_from >= bonusEndBlock) {
-            return _to.sub(_from);
-        } else {
-            return
-                bonusEndBlock.sub(_from).mul(BONUS_MULTIPLIER).add(
-                    _to.sub(bonusEndBlock)
-                );
-        }
+    // Return reward multiplier over the given _from to _to timestamp.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
     // View function to see pending JOEs on frontend.
-    function pendingJoe(uint256 _pid, address _user)
-        external
-        view
-        returns (uint256)
-    {
+    function pendingJoe(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
         uint256 accJoePerShare = pool.accJoePerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
-        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
-            uint256 multiplier =
-                getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 joeReward =
-                multiplier.mul(joePerBlock).mul(pool.allocPoint).div(
-                    totalAllocPoint
-                );
-            accJoePerShare = accJoePerShare.add(
-                joeReward.mul(1e12).div(lpSupply)
-            );
+        if (block.timestamp > pool.lastRewardTimestamp && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardTimestamp, block.timestamp);
+            uint256 joeReward = multiplier.mul(joePerSec).mul(pool.allocPoint).div(totalAllocPoint);
+            accJoePerShare = accJoePerShare.add(joeReward.mul(1e12).div(lpSupply));
         }
         return user.amount.mul(accJoePerShare).div(1e12).sub(user.rewardDebt);
     }
 
-    // Update reward vairables for all pools. Be careful of gas spending!
+    // Update reward variables for all pools. Be careful of gas spending!
     function massUpdatePools() public {
         uint256 length = poolInfo.length;
         for (uint256 pid = 0; pid < length; ++pid) {
@@ -206,28 +163,25 @@ contract MasterChef is Ownable {
         }
     }
 
+
     // Update reward variables of the given pool to be up-to-date.
     function updatePool(uint256 _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
-        if (block.number <= pool.lastRewardBlock) {
+        if (block.timestamp <= pool.lastRewardTimestamp) {
             return;
         }
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (lpSupply == 0) {
-            pool.lastRewardBlock = block.number;
+            pool.lastRewardTimestamp = block.timestamp;
             return;
         }
-        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 joeReward =
-            multiplier.mul(joePerBlock).mul(pool.allocPoint).div(
-                totalAllocPoint
-            );
-        joe.mint(devaddr, joeReward.div(10));
-        joe.mint(address(this), joeReward);
-        pool.accJoePerShare = pool.accJoePerShare.add(
-            joeReward.mul(1e12).div(lpSupply)
-        );
-        pool.lastRewardBlock = block.number;
+        uint256 multiplier = getMultiplier(pool.lastRewardTimestamp, block.timestamp);
+        uint256 joeReward = multiplier.mul(joePerSec).mul(pool.allocPoint).div(totalAllocPoint);
+        joe.mint(devaddr, joeReward.mul(20).div(100));
+        joe.mint(treasuryaddr, joeReward.mul(20).div(100));
+        joe.mint(address(this), joeReward.mul(60).div(100));
+        pool.accJoePerShare = pool.accJoePerShare.add(joeReward.mul(1e12).div(lpSupply));
+        pool.lastRewardTimestamp = block.timestamp;
     }
 
     // Deposit LP tokens to MasterChef for JOE allocation.
@@ -236,17 +190,10 @@ contract MasterChef is Ownable {
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending =
-                user.amount.mul(pool.accJoePerShare).div(1e12).sub(
-                    user.rewardDebt
-                );
+            uint256 pending = user.amount.mul(pool.accJoePerShare).div(1e12).sub(user.rewardDebt);
             safeJoeTransfer(msg.sender, pending);
         }
-        pool.lpToken.safeTransferFrom(
-            address(msg.sender),
-            address(this),
-            _amount
-        );
+        pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
         user.amount = user.amount.add(_amount);
         user.rewardDebt = user.amount.mul(pool.accJoePerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
@@ -257,15 +204,13 @@ contract MasterChef is Ownable {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
+
         updatePool(_pid);
-        uint256 pending =
-            user.amount.mul(pool.accJoePerShare).div(1e12).sub(
-                user.rewardDebt
-            );
+        uint256 pending = user.amount.mul(pool.accJoePerShare).div(1e12).sub(user.rewardDebt);
         safeJoeTransfer(msg.sender, pending);
         user.amount = user.amount.sub(_amount);
-        user.rewardDebt = user.amount.mul(pool.accJoePerShare).div(1e12);
         pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        user.rewardDebt = user.amount.mul(pool.accJoePerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -293,5 +238,14 @@ contract MasterChef is Ownable {
     function dev(address _devaddr) public {
         require(msg.sender == devaddr, "dev: wut?");
         devaddr = _devaddr;
+        emit SetDevAddress(msg.sender, _devaddr);
+    }
+
+    // Pancake has to add hidden dummy pools inorder to alter the emission,
+    // here we make it simple and transparent to all.
+    function updateEmissionRate(uint256 _joePerSec) public onlyOwner {
+        massUpdatePools();
+        joePerSec = _joePerSec;
+        emit UpdateEmissionRate(msg.sender, _joePerSec);
     }
 }
