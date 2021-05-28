fb520bc9dd10840ef6471489d210fd4574aef415
cryptofish7 <cryptofish7@protonmail.com>
2021-05-28 00:04:24 -0400

    Remove unused mocks

--- contracts/mocks/JoeSwapFactoryMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.6.12;
-
-import "../traderjoe/JoeFactory.sol";
-
-contract JoeSwapFactoryMock is JoeFactory {
-    constructor(address _feeToSetter) public JoeFactory(_feeToSetter) {}
-}
--- contracts/mocks/JoeSwapPairMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.6.12;
-
-import "../traderjoe/JoePair.sol";
-
-contract JoeSwapPairMock is JoePair {
-    constructor() public JoePair() {}
-}
