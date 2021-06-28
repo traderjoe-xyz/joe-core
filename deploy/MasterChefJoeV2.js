0c59f68a766354c84a4ce9c062b017989be4b188
cryptofish7 <cryptofish7@protonmail.com>
2021-06-28 12:15:11 -0400

    Add avalanche deployments

--- deploy/MasterChefJoeV2.js
+++ deploy/MasterChefJoeV2.js
@@ -1,7 +1,7 @@
 module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   const { deploy } = deployments;
 
-  const { deployer, dev, treasury } = await getNamedAccounts();
+  const { deployer, dev, treasury, investor } = await getNamedAccounts();
 
   const joe = await ethers.getContract("JoeToken");
 
@@ -11,20 +11,22 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
       joe.address,
       dev,
       treasury,
+      investor,
       "30000000000000000000", // 30 JOE per sec
-      "1625335200", // Sat Jul 03 10:00
+      "1625320800", // Sat Jul 03 10:00
       "200", // 20%
       "200", // 20%
+      "100", // 10%
     ],
     log: true,
     deterministicDeployment: false,
   });
 
-  // if ((await joe.owner()) !== address) {
-  //   // Transfer Joe Ownership to MasterChefJoeV2
-  //   console.log("Transfer Joe Ownership to MasterChefJoeV2");
-  //   await (await joe.transferOwnership(address)).wait();
-  // }
+  if ((await joe.owner()) !== address) {
+    // Transfer Joe Ownership to MasterChefJoeV2
+    console.log("Transfer Joe Ownership to MasterChefJoeV2");
+    await (await joe.transferOwnership(address)).wait();
+  }
 };
 
 module.exports.tags = ["MasterChefJoeV2", "chef"];
