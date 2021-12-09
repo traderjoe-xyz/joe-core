4a0dce4b9e4ca9f8a4678e8a189e12aec433432c
intro0000 <hadionoc@gmail.com>
2021-12-09 01:01:23 -0500

    JoeMakerV3 + test

--- contracts/JoeMakerV3.sol
+++ contracts/JoeMakerV3.sol
@@ -12,7 +12,7 @@ import "./traderjoe/interfaces/IJoeFactory.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// JoeMakerV3 is MasterJoe's left hand and kinda a wizard. He can cook up Joe from pretty much anything!
+// JoeMakerV2 is MasterJoe's left hand and kinda a wizard. He can cook up Joe from pretty much anything!
 // This contract handles "serving up" rewards for xJoe holders by trading tokens collected from fees for Joe.
 
 // T1 - T4: OK
@@ -25,7 +25,7 @@ contract JoeMakerV3 is Ownable {
     address public immutable bar;
     address private immutable joe;
     address private immutable wavax;
-    uint256 public devCut = 0; // in basis points aka parts per 10,000 so 5000 is 50%, cap of 50%, default is 0
+    uint public devCut = 0;  // in basis points aka parts per 10,000 so 5000 is 50%, cap of 50%, default is 0
     address public devAddr;
 
     // set of addresses that can perform certain functions
@@ -42,7 +42,7 @@ contract JoeMakerV3 is Ownable {
     mapping(address => address) internal _bridges;
 
     event SetDevAddr(address _addr);
-    event SetDevCut(uint256 _amount);
+    event SetDevCut(uint _amount);
     event LogBridgeSet(address indexed token, address indexed bridge);
     event LogConvert(
         address indexed server,
@@ -92,7 +92,7 @@ contract JoeMakerV3 is Ownable {
         emit LogBridgeSet(token, bridge);
     }
 
-    function setDevCut(uint256 _amount) external onlyOwner {
+    function setDevCut(uint _amount) external onlyOwner {
         require(_amount <= 5000, "setDevCut: cut too high");
         devCut = _amount;
 
@@ -105,7 +105,6 @@ contract JoeMakerV3 is Ownable {
 
         emit SetDevAddr(_addr);
     }
-
     // End owner functions
 
     function bridgeFor(address token) public view returns (address bridge) {
@@ -128,14 +127,17 @@ contract JoeMakerV3 is Ownable {
     //     As the size of the JoeBar has grown, this requires large amounts of funds and isn't super profitable anymore
     //     The onlyEOA modifier prevents this being done with a flash loan.
     // C1 - C24: OK
-    function convert(address token0, address token1) external onlyEOA onlyAuth {
+    function convert(address token0, address token1) external onlyEOA() onlyAuth() {
         _convert(token0, token1);
     }
 
     // F1 - F10: OK, see convert
     // C1 - C24: OK
     // C3: Loop is under control of the caller
-    function convertMultiple(address[] calldata token0, address[] calldata token1) external onlyEOA onlyAuth {
+    function convertMultiple(
+        address[] calldata token0,
+        address[] calldata token1
+    ) external onlyEOA() onlyAuth() {
         // TODO: This can be optimized a fair bit, but this is safer and simpler for now
         uint256 len = token0.length;
         for (uint256 i = 0; i < len; i++) {
@@ -150,18 +152,23 @@ contract JoeMakerV3 is Ownable {
         uint256 amount1;
 
         // handle case where non-LP tokens need to be converted
-        if (token0 == token1) {
+        if(token0 == token1) {
             amount0 = IERC20(token0).balanceOf(address(this));
             amount1 = 0;
-        } else {
+        }
+        else {
+
             IJoePair pair = IJoePair(factory.getPair(token0, token1));
             require(address(pair) != address(0), "JoeMakerV3: Invalid pair");
 
-            IERC20(address(pair)).safeTransfer(address(pair), pair.balanceOf(address(this)));
+            IERC20(address(pair)).safeTransfer(
+                address(pair),
+                pair.balanceOf(address(this))
+            );
 
             // take balance of tokens in this contract before burning the pair, incase there are already some here
-            uint256 tok0bal = IERC20(token0).balanceOf(address(this));
-            uint256 tok1bal = IERC20(token1).balanceOf(address(this));
+            uint tok0bal = IERC20(token0).balanceOf(address(this));
+            uint tok1bal = IERC20(token1).balanceOf(address(this));
 
             pair.burn(address(this));
 
@@ -245,22 +252,21 @@ contract JoeMakerV3 is Ownable {
         require(address(pair) != address(0), "JoeMakerV3: Cannot convert");
 
         (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
-        (uint256 reserveInput, uint256 reserveOutput) = fromToken == pair.token0()
-            ? (reserve0, reserve1)
-            : (reserve1, reserve0);
+        (uint reserveInput, uint reserveOutput) = fromToken == pair.token0() ? (reserve0, reserve1) : (reserve1, reserve0);
         IERC20(fromToken).safeTransfer(address(pair), amountIn);
-        uint256 amountInput = IERC20(fromToken).balanceOf(address(pair)).sub(reserveInput); // calculate amount that was transferred, this accounts for transfer taxes
+        uint amountInput = IERC20(fromToken).balanceOf(address(pair)).sub(reserveInput); // calculate amount that was transferred, this accounts for transfer taxes
 
         amountOut = getAmountOut(amountInput, reserveInput, reserveOutput);
-        (uint256 amount0Out, uint256 amount1Out) = fromToken == pair.token0()
-            ? (uint256(0), amountOut)
-            : (amountOut, uint256(0));
+        (uint amount0Out, uint amount1Out) = fromToken == pair.token0() ? (uint(0), amountOut) : (amountOut, uint(0));
         pair.swap(amount0Out, amount1Out, to, new bytes(0));
     }
 
     // F1 - F10: OK
     // C1 - C24: OK
-    function _toJOE(address token, uint256 amountIn) internal returns (uint256 amountOut) {
+    function _toJOE(address token, uint256 amountIn)
+    internal
+    returns (uint256 amountOut)
+    {
         uint256 amount = amountIn;
         if (devCut > 0) {
             amount = amount.mul(devCut).div(10000);
@@ -270,16 +276,12 @@ contract JoeMakerV3 is Ownable {
         amountOut = _swap(token, joe, amount, bar);
     }
 
-    function getAmountOut(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) internal pure returns (uint256 amountOut) {
-        require(amountIn > 0, "JoeMakerV3: INSUFFICIENT_INPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "JoeMakerV3: INSUFFICIENT_LIQUIDITY");
-        uint256 amountInWithFee = amountIn.mul(997);
-        uint256 numerator = amountInWithFee.mul(reserveOut);
-        uint256 denominator = reserveIn.mul(1000).add(amountInWithFee);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+        require(amountIn > 0, 'JoeMakerV3: INSUFFICIENT_INPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'JoeMakerV3: INSUFFICIENT_LIQUIDITY');
+        uint amountInWithFee = amountIn.mul(997);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
         amountOut = numerator / denominator;
     }
 }
