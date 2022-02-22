621cbdfc062063ff1ca6e37c9aaa4310c12b9b14
Lint Action <lint-action@samuelmeuli.com>
2022-02-22 17:24:36 +0000

    Fix code style issues with Prettier

--- contracts/MoneyMaker.sol
+++ contracts/MoneyMaker.sol
@@ -109,10 +109,7 @@ contract MoneyMaker is Ownable {
     /// @param bridge The address of the tokenTo
     function setBridge(address token, address bridge) external onlyAuth {
         // Checks
-        require(
-            token != tokenTo && token != wavax && token != bridge,
-            "MoneyMaker: Invalid bridge"
-        );
+        require(token != tokenTo && token != wavax && token != bridge, "MoneyMaker: Invalid bridge");
 
         // Effects
         _bridges[token] = bridge;
@@ -131,10 +128,7 @@ contract MoneyMaker is Ownable {
     /// @notice Sets `devAddr`, the address that will receive the `devCut`
     /// @param _addr The new dev address
     function setDevAddr(address _addr) external onlyOwner {
-        require(
-            _addr != address(0),
-            "setDevAddr, address cannot be zero address"
-        );
+        require(_addr != address(0), "setDevAddr, address cannot be zero address");
         devAddr = _addr;
 
         emit SetDevAddr(_addr);
@@ -143,10 +137,7 @@ contract MoneyMaker is Ownable {
     /// @notice Sets token that we're buying back
     /// @param _tokenTo The new token address
     function setTokenToAddress(address _tokenTo) external onlyOwner {
-        require(
-            _tokenTo != address(0),
-            "setTokenToAddress, address cannot be zero address"
-        );
+        require(_tokenTo != address(0), "setTokenToAddress, address cannot be zero address");
         tokenTo = _tokenTo;
 
         emit SetTokenTo(_tokenTo);
@@ -179,10 +170,7 @@ contract MoneyMaker is Ownable {
         address token1,
         uint256 slippage
     ) external onlyEOA onlyAuth {
-        require(
-            slippage < 5_000,
-            "MoneyMaker: slippage needs to be lower than 50%"
-        );
+        require(slippage < 5_000, "MoneyMaker: slippage needs to be lower than 50%");
         _convert(token0, token1, slippage);
     }
 
@@ -197,10 +185,7 @@ contract MoneyMaker is Ownable {
         uint256 slippage
     ) external onlyEOA onlyAuth {
         // TODO: This can be optimized a fair bit, but this is safer and simpler for now
-        require(
-            slippage < 5_000,
-            "MoneyMaker: slippage needs to be lower than 50%"
-        );
+        require(slippage < 5_000, "MoneyMaker: slippage needs to be lower than 50%");
 
         uint256 len = token0.length;
         for (uint256 i = 0; i < len; i++) {
@@ -229,10 +214,7 @@ contract MoneyMaker is Ownable {
             IJoePair pair = IJoePair(factory.getPair(token0, token1));
             require(address(pair) != address(0), "MoneyMaker: Invalid pair");
 
-            IERC20(address(pair)).safeTransfer(
-                address(pair),
-                pair.balanceOf(address(this))
-            );
+            IERC20(address(pair)).safeTransfer(address(pair), pair.balanceOf(address(this)));
 
             // take balance of tokens in this contract before burning the pair, incase there are already some here
             uint256 tok0bal = IERC20(token0).balanceOf(address(this));
@@ -292,22 +274,10 @@ contract MoneyMaker is Ownable {
             tokenOut = _toToken(token0, amount0, slippage).add(amount1);
         } else if (token0 == wavax) {
             // eg. AVAX - USDC
-            tokenOut = _toToken(
-                wavax,
-                _swap(token1, wavax, amount1, address(this), slippage).add(
-                    amount0
-                ),
-                slippage
-            );
+            tokenOut = _toToken(wavax, _swap(token1, wavax, amount1, address(this), slippage).add(amount0), slippage);
         } else if (token1 == wavax) {
             // eg. USDT - AVAX
-            tokenOut = _toToken(
-                wavax,
-                _swap(token0, wavax, amount0, address(this), slippage).add(
-                    amount1
-                ),
-                slippage
-            );
+            tokenOut = _toToken(wavax, _swap(token0, wavax, amount0, address(this), slippage).add(amount1), slippage);
         } else {
             // eg. MIC - USDT
             address bridge0 = bridgeFor(token0);
@@ -363,14 +333,11 @@ contract MoneyMaker is Ownable {
         require(address(pair) != address(0), "MoneyMaker: Cannot convert");
 
         (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
-        (uint256 reserveInput, uint256 reserveOutput) = fromToken ==
-            pair.token0()
+        (uint256 reserveInput, uint256 reserveOutput) = fromToken == pair.token0()
             ? (reserve0, reserve1)
             : (reserve1, reserve0);
         IERC20(fromToken).safeTransfer(address(pair), amountIn);
-        uint256 amountInput = IERC20(fromToken).balanceOf(address(pair)).sub(
-            reserveInput
-        ); // calculate amount that was transferred, this accounts for transfer taxes
+        uint256 amountInput = IERC20(fromToken).balanceOf(address(pair)).sub(reserveInput); // calculate amount that was transferred, this accounts for transfer taxes
 
         amountOut = getAmountOut(amountInput, reserveInput, reserveOutput);
 
@@ -423,10 +390,7 @@ contract MoneyMaker is Ownable {
         uint256 reserveOut
     ) internal pure returns (uint256 amountOut) {
         require(amountIn > 0, "MoneyMaker: INSUFFICIENT_INPUT_AMOUNT");
-        require(
-            reserveIn > 0 && reserveOut > 0,
-            "MoneyMaker: INSUFFICIENT_LIQUIDITY"
-        );
+        require(reserveIn > 0 && reserveOut > 0, "MoneyMaker: INSUFFICIENT_LIQUIDITY");
         uint256 amountInWithFee = amountIn.mul(997);
         uint256 numerator = amountInWithFee.mul(reserveOut);
         uint256 denominator = reserveIn.mul(1000).add(amountInWithFee);
