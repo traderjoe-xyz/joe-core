440ab369c65af0eb7ddc9e6f287b36a7ecf0790d
Louis Myself <louismemyself@gmail.com>
2022-02-22 18:25:31 +0100

    removing optimizer for 0.7.6

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -273,12 +273,7 @@ const config: HardhatUserConfig = {
       },
       {
         version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+        settings: {},
       },
     ],
   },
