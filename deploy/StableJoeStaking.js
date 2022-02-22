24e195010f8478b08eed199dd14c14b8b9a109c5
jummy123 <jummy123@users.noreply.github.com>
2022-02-22 16:38:13 +0000

    Set reward tokens for `StableJoeStaking`

--- deploy/StableJoeStaking.js
+++ deploy/StableJoeStaking.js
@@ -8,15 +8,18 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   if (chainId == 4) {
     // rinkeby contract addresses
     joeAddress = "0xce347E069B68C53A9ED5e7DA5952529cAF8ACCd4";
+    // fish token
+    rewardToken = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
     feeCollector = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
     depositFeePercent = 0;
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     joeAddress = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd";
+    // USDC.e
+    rewardToken = "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664";
     feeCollector = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
     depositFeePercent = 0;
   }
-  rewardToken = joeAddress;
 
   const stableJoeStaking = await deploy("StableJoeStaking", {
     from: deployer,
