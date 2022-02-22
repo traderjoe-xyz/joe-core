3466a63cff12335b2d1c4268fa4fae0c28f0c110
jummy123 <jummy123@users.noreply.github.com>
2022-02-22 15:47:56 +0000

    use JOE as initial reward token

--- deploy/StableJoeStaking.js
+++ deploy/StableJoeStaking.js
@@ -8,16 +8,15 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   if (chainId == 4) {
     // rinkeby contract addresses
     joeAddress = "0xce347E069B68C53A9ED5e7DA5952529cAF8ACCd4";
-    rewardToken = ethers.constants.AddressZero;
     feeCollector = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
     depositFeePercent = 0;
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     joeAddress = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd";
-    rewardToken = ethers.constants.AddressZero;
     feeCollector = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
     depositFeePercent = 0;
   }
+  rewardToken = joeAddress;
 
   const stableJoeStaking = await deploy("StableJoeStaking", {
     from: deployer,
