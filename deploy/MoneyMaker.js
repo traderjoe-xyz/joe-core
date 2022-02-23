dc70c98e422bc2967b7817ff5d345e539357115e
Lint Action <lint-action@samuelmeuli.com>
2022-02-23 16:44:06 +0000

    Fix code style issues with Prettier

--- deploy/MoneyMaker.js
+++ deploy/MoneyMaker.js
@@ -9,7 +9,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // rinkeby contract addresses
     factoryAddress = "0x86f83be9770894d8e46301b12E88e14AdC6cdb5F";
     rewardTokenAddress = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
-    wavaxAddress = "0xc778417e063141139fce010982780140aa0cd5ab";  // WETH
+    wavaxAddress = "0xc778417e063141139fce010982780140aa0cd5ab"; // WETH
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     factoryAddress = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
@@ -21,15 +21,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   await deploy("MoneyMaker", {
     from: deployer,
-    args: [
-      factoryAddress,
-      stakingAddress,
-      rewardTokenAddress,
-      wavaxAddress,
-    ],
+    args: [factoryAddress, stakingAddress, rewardTokenAddress, wavaxAddress],
     log: true,
   });
 };
 module.exports.tags = ["MoneyMaker"];
 module.exports.dependencies = ["StableJoeStaking"];
-
