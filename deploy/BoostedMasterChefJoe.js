7a99e0c7169adb7f19888f38b2f2829fff86d6f0
jummy123 <jummy123@users.noreply.github.com>
2022-03-01 00:55:50 +0000

    Split token creation to seperate file

--- deploy/BoostedMasterChefJoe.js
+++ deploy/BoostedMasterChefJoe.js
@@ -5,32 +5,23 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   let PID,
     joeAddress,
     veJoeAddress = (await deployments.get("VeJoeToken")).address,
+    dummyTokenAddress = (await deployments.get("BoostedMasterChefToken")).address,
     masterChefV2Address;
 
-  const DummyToken = await deploy("ERC20Mock", {
-    from: deployer,
-    args: ["Joe Boost Dummy Token", "BDUMMY", "1"],
-    log: true,
-  });
-
-  const dummyToken = await ethers.getContract("ERC20Mock", DummyToken.address);
-  await dummyToken.renounceOwnership();
-
+  const chainId = await getChainId();
   if (chainId == 4) {
     // rinkeby contract addresses
     joeAddress = "0xce347E069B68C53A9ED5e7DA5952529cAF8ACCd4";
     masterChefV2Address = "0x1F51b7697A1919cF301845c93D4843FD620ad7Cc";
+    /// XXX Pid needs to be pre-created.
+    PID = 0;
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     joeAddress = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd";
     masterChefV2Address = "0xC98C3C547DDbcc0029F38E0383C645C202aD663d";
+    /// XXX
+    PID = 0;
   }
-
-  const MCV2 = await ethers.getContract("MasterChefJoeV2", masterChefV2Address);
-  await (await MCV2.add(100, dummyToken.address, false)).wait();
-
-  // PID should be the last added pool.
-  PID = await MCV2.poolLength;
 
   const { address } = await deploy("BoostedMasterChefJoe", {
     from: deployer,
@@ -46,13 +37,17 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
     log: true,
   });
 
-  const boostedMasterChef = await ethers.getContract(
-    "BoostedMasterChefJoe",
-    address
-  );
-  await (await dummyToken.approve(boostedMasterChef.address, 1)).wait();
-  await (await BoostedMasterChefJoe.init(dummyToken.address)).wait();
+  //const dummyToken = await ethers.getContractAt(
+  //  "ERC20Mock",
+  //  dummyTokenAddress,
+  //);
+  //const boostedMasterChef = await ethers.getContractAt(
+  //  "BoostedMasterChefJoe",
+  //  address
+  //);
+  //await dummyToken.approve(boostedMasterChef.address, "1");
+  //await BoostedMasterChefJoe.init(dummyToken.address);
 };
 
 module.exports.tags = ["BoostedMasterChefJoe"];
-module.exports.dependencies = ["VeJoeToken"];
+module.exports.dependencies = ["BoostedMasterChefToken", "VeJoeToken"];
