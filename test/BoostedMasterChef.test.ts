0914eeeb9960b1c51a5c138df7ef93625437cc07
jummy123 <jummy123@users.noreply.github.com>
2022-03-07 11:42:23 +0000

    Fix divide by zero if contract sent tokens manually

--- test/BoostedMasterChef.test.ts
+++ test/BoostedMasterChef.test.ts
@@ -338,6 +338,13 @@ describe("BoostedMasterChefJoe", function () {
     expect((await this.bmc.poolInfo(0)).allocPoint).to.equal(1000)
   })
 
+  it("it should allow deposits if contract has balance", async function () {
+    await this.lp.transfer(this.bmc.address, 100);
+    await this.bmc.updatePool(0);
+    await this.lp.connect(this.bob).approve(this.bmc.address, 100);
+    await this.bmc.connect(this.bob).deposit(0, 100);
+  })
+
   after(async function () {
     await network.provider.request({
       method: "hardhat_reset",
