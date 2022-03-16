f8e082938b8f378c9d22604c9b44964dfcf0dbe7
jummy123 <93708655+jummy123@users.noreply.github.com>
2022-03-16 23:01:53 +0000

    `VeJoeToken` and `VeJoeStaking` mainnet deploy (#79)
    * `VeJoeToken` and `VeJoeStaking` mainnet deploy
    
    * Fix code style issues with Prettier
    
    Co-authored-by: jummy123 <jummy123@users.noreply.github.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- deploy/VeJoeStaking.js
+++ deploy/VeJoeStaking.js
@@ -1,5 +1,5 @@
 module.exports = async function ({ getNamedAccounts, deployments }) {
-  const { deploy } = deployments;
+  const { deploy, catchUnknownSigner } = deployments;
   const { deployer } = await getNamedAccounts();
 
   let joeAddress,
@@ -8,7 +8,8 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     speedUpVeJoePerSharePerSec = 3170979198376,
     speedUpThreshold = 5,
     speedUpDuration = 15 * 60 * 60 * 24,
-    maxCapPct = 10000;
+    maxCapPct = 10000,
+    proxyOwner;
 
   const chainId = await getChainId();
   if (chainId == 4) {
@@ -16,34 +17,41 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     joeAddress = ethers.utils.getAddress(
       "0xce347E069B68C53A9ED5e7DA5952529cAF8ACCd4"
     );
+    proxyOwner = deployer.address;
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     joeAddress = ethers.utils.getAddress(
       "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd"
     );
+    // multisig
+    proxyOwner = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
   }
 
-  const stableJoeStaking = await deploy("VeJoeStaking", {
-    from: deployer,
-    proxy: {
-      proxyContract: "OpenZeppelinTransparentProxy",
-      execute: {
-        init: {
-          methodName: "initialize",
-          args: [
-            joeAddress,
-            veJoeAddress,
-            veJoePerSharePerSec,
-            speedUpVeJoePerSharePerSec,
-            speedUpThreshold,
-            speedUpDuration,
-            maxCapPct,
-          ],
+  await catchUnknownSigner(
+    deploy("VeJoeStaking", {
+      from: deployer,
+      proxy: {
+        owner: proxyOwner,
+        proxyContract: "OpenZeppelinTransparentProxy",
+        viaAdminContract: "DefaultProxyAdmin",
+        execute: {
+          init: {
+            methodName: "initialize",
+            args: [
+              joeAddress,
+              veJoeAddress,
+              veJoePerSharePerSec,
+              speedUpVeJoePerSharePerSec,
+              speedUpThreshold,
+              speedUpDuration,
+              maxCapPct,
+            ],
+          },
         },
       },
-    },
-    log: true,
-  });
+      log: true,
+    })
+  );
 };
 
 module.exports.tags = ["VeJoeStaking"];
