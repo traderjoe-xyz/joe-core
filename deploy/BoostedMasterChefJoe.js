40abc4749804b6476fe5fd80864b096352bb6899
jummy123 <93708655+jummy123@users.noreply.github.com>
2022-03-17 00:38:02 +0000

    Use proxy owner for `BoostedMasterChefJoe` (#81)
    * Use proxy owner for `BoostedMasterChefJoe`
    
    * Fix code style issues with Prettier
    
    Co-authored-by: jummy123 <jummy123@users.noreply.github.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- deploy/BoostedMasterChefJoe.js
+++ deploy/BoostedMasterChefJoe.js
@@ -1,5 +1,5 @@
 module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
-  const { deploy } = deployments;
+  const { deploy, catchUnknownSigner } = deployments;
   const { deployer } = await getNamedAccounts();
 
   let PID,
@@ -7,7 +7,9 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
     veJoeAddress = (await deployments.get("VeJoeToken")).address,
     dummyTokenAddress = (await deployments.get("BoostedMasterChefToken"))
       .address,
-    masterChefV2Address;
+    masterChefV2Address,
+    proxyOwner,
+    bmcj;
 
   const chainId = await getChainId();
   if (chainId == 4) {
@@ -15,26 +17,32 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
     joeAddress = "0xce347E069B68C53A9ED5e7DA5952529cAF8ACCd4";
     masterChefV2Address = "0x1F51b7697A1919cF301845c93D4843FD620ad7Cc";
     PID = 8;
+    proxyOwner = deployer.address;
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     joeAddress = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd";
     masterChefV2Address = "0xd6a4F121CA35509aF06A0Be99093d08462f53052";
     /// XXX Pid needs to be pre-created.
     PID = 0;
+    proxyOwner = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
   }
 
-  const bmcj = await deploy("BoostedMasterChefJoe", {
-    from: deployer,
-    proxy: {
-      proxyContract: "OpenZeppelinTransparentProxy",
-      execute: {
-        init: {
-          methodName: "initialize",
-          args: [masterChefV2Address, joeAddress, veJoeAddress, PID],
+  await catchUnknownSigner(async () => {
+    bmcj = await deploy("BoostedMasterChefJoe", {
+      from: deployer,
+      proxy: {
+        owner: proxyOwner,
+        proxyContract: "OpenZeppelinTransparentProxy",
+        viaAdminContract: "DefaultProxyAdmin",
+        execute: {
+          init: {
+            methodName: "initialize",
+            args: [masterChefV2Address, joeAddress, veJoeAddress, PID],
+          },
         },
       },
-    },
-    log: true,
+      log: true,
+    });
   });
   if (bmcj.newlyDeployed) {
     const dummyToken = await ethers.getContractAt(
