d6dd3f8d378df1a7274d996fabbbe537733fb30a
jummy123 <jummy123@users.noreply.github.com>
2022-03-31 16:39:33 +0100

    Update `StableJoeStaking` implementation

--- deploy/StableJoeStaking.js
+++ deploy/StableJoeStaking.js
@@ -2,7 +2,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, catchUnknownSigner } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  let rewardToken, joeAddress, feeCollector, depositFeePercent;
+  let rewardToken, joeAddress, feeCollector, depositFeePercent, proxyOwner;
 
   const chainId = await getChainId();
   if (chainId == 4) {
@@ -12,6 +12,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     rewardToken = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
     feeCollector = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
     depositFeePercent = 0;
+    proxyOwner = deployer.address;
   } else if (chainId == 43114 || chainId == 31337) {
     // avalanche mainnet or hardhat network addresses
     joeAddress = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd";
@@ -19,12 +20,14 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     rewardToken = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";
     feeCollector = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
     depositFeePercent = 0;
+    proxyOwner = "0x2fbB61a10B96254900C03F1644E9e1d2f5E76DD2";
   }
 
   await catchUnknownSigner(
     deploy("StableJoeStaking", {
       from: deployer,
       proxy: {
+        owner: proxyOwner,
         proxyContract: "OpenZeppelinTransparentProxy",
         execute: {
           init: {
