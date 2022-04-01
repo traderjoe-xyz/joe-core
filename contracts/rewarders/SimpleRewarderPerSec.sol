6c7f016e01ab5981580652368b181a248aefd17d
Louis Myself <louismemyself@gmail.com>
2022-04-01 15:21:32 +0200

    gas opt
    Fix code style issues with Prettier
    
    struct

--- contracts/rewarders/SimpleRewarderPerSec.sol
+++ contracts/rewarders/SimpleRewarderPerSec.sol
@@ -62,11 +62,6 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public immutable override rewardToken;
-    IERC20 public immutable lpToken;
-    bool public immutable isNative;
-    IMasterChefJoe public immutable MCJ;
-
     /// @notice Info of each MCJ user.
     /// `amount` LP token amount the user has provided.
     /// `rewardDebt` The amount of YOUR_TOKEN entitled to the user.
@@ -84,14 +79,38 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
         uint256 lastRewardTimestamp;
     }
 
+    IERC20 public immutable override rewardToken;
+    IERC20 public immutable lpToken;
+    bool public immutable isNative;
+    IMasterChefJoe public immutable MCJ;
+    uint256 public tokenPerSec;
+
+    // Given the fraction, tokenReward * ACC_TOKEN_PRECISION / lpSupply, we consider
+    // several edge cases.
+    //
+    // Edge case n1: maximize the numerator, minimize the denominator.
+    // `lpSupply` = 1 WEI
+    // `tokenPerSec` = 1e(30)
+    // `timeElapsed` = 31 years, i.e. 1e9 seconds
+    // result = 1e9 * 1e30 * 1e36 / 1
+    //        = 1e75
+    // (No overflow as max uint256 is 1.15e77).
+    // PS: This will overflow when `timeElapsed` becomes greater than 1e11, i.e. in more than 3_000 years
+    // so it should be fine.
+    //
+    // Edge case n2: minimize the numerator, maximize the denominator.
+    // `lpSupply` = max(uint112) = 1e34
+    // `tokenPerSec` = 1 WEI
+    // `timeElapsed` = 1 second
+    // result = 1 * 1 * 1e36 / 1e34
+    //        = 1e2
+    // (Not rounded to zero, therefore ACC_TOKEN_PRECISION = 1e36 is safe)
+    uint256 private constant ACC_TOKEN_PRECISION = 1e36;
+
     /// @notice Info of the poolInfo.
     PoolInfo public poolInfo;
     /// @notice Info of each user that stakes LP tokens.
     mapping(address => UserInfo) public userInfo;
-
-    uint256 public tokenPerSec;
-
-    uint256 private ACC_TOKEN_PRECISION;
 
     event OnReward(address indexed user, uint256 amount);
     event RewardRateUpdated(uint256 oldRate, uint256 newRate);
@@ -119,59 +138,10 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
         MCJ = _MCJ;
         isNative = _isNative;
         poolInfo = PoolInfo({lastRewardTimestamp: block.timestamp, accTokenPerShare: 0});
-
-        // Given the fraction, tokenReward * ACC_TOKEN_PRECISION / lpSupply, we consider
-        // several edge cases.
-
-        // Edge case n1: maximize the numerator, minimize the denominator.
-        // `lpSupply` = 1 WEI
-        // `tokenPerSec` = 1e(30)
-        // `timeElapsed` = 31 years, i.e. 1e9 seconds
-        // result = 1e9 * 1e30 * 1e36 / 1
-        //        = 1e75
-        // (No overflow as max uint256 is 1.15e77).
-        // PS: This will overflow when `timeElapsed` becomes greater than 1e11, i.e. in more than 3_000 years
-        // so it should be fine.
-        //
-        // Edge case n2: minimize the numerator, maximize the denominator.
-        // `lpSupply` = max(uint112) = 1e34
-        // `tokenPerSec` = 1 WEI
-        // `timeElapsed` = 1 second
-        // result = 1 * 1 * 1e36 / 1e34
-        //        = 1e2
-        // (Not rounded to zero, therefore ACC_TOKEN_PRECISION = 1e36 is safe)
-        ACC_TOKEN_PRECISION = 1e36;
     }
 
-    /// @notice Update reward variables of the given poolInfo.
-    /// @return pool Returns the pool that was updated.
-    function updatePool() public returns (PoolInfo memory pool) {
-        pool = poolInfo;
-
-        if (block.timestamp > pool.lastRewardTimestamp) {
-            uint256 lpSupply = lpToken.balanceOf(address(MCJ));
-
-            if (lpSupply > 0) {
-                uint256 timeElapsed = block.timestamp.sub(pool.lastRewardTimestamp);
-                uint256 tokenReward = timeElapsed.mul(tokenPerSec);
-                pool.accTokenPerShare = pool.accTokenPerShare.add((tokenReward.mul(ACC_TOKEN_PRECISION) / lpSupply));
-            }
-
-            pool.lastRewardTimestamp = block.timestamp;
-            poolInfo = pool;
-        }
-    }
-
-    /// @notice Sets the distribution reward rate. This will also update the poolInfo.
-    /// @param _tokenPerSec The number of tokens to distribute per second
-    function setRewardRate(uint256 _tokenPerSec) external onlyOwner {
-        updatePool();
-
-        uint256 oldRate = tokenPerSec;
-        tokenPerSec = _tokenPerSec;
-
-        emit RewardRateUpdated(oldRate, _tokenPerSec);
-    }
+    /// @notice payable function needed to receive AVAX
+    receive() external payable {}
 
     /// @notice Function called by MasterChefJoe whenever staker claims JOE harvest. Allows staker to also receive a 2nd reward token.
     /// @param _user Address of user
@@ -235,6 +205,45 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
         );
     }
 
+    /// @notice View function to see balance of reward token.
+    function balance() external view returns (uint256) {
+        if (isNative) {
+            return address(this).balance;
+        } else {
+            return rewardToken.balanceOf(address(this));
+        }
+    }
+
+    /// @notice Sets the distribution reward rate. This will also update the poolInfo.
+    /// @param _tokenPerSec The number of tokens to distribute per second
+    function setRewardRate(uint256 _tokenPerSec) external onlyOwner {
+        updatePool();
+
+        uint256 oldRate = tokenPerSec;
+        tokenPerSec = _tokenPerSec;
+
+        emit RewardRateUpdated(oldRate, _tokenPerSec);
+    }
+
+    /// @notice Update reward variables of the given poolInfo.
+    /// @return pool Returns the pool that was updated.
+    function updatePool() public returns (PoolInfo memory pool) {
+        pool = poolInfo;
+
+        if (block.timestamp > pool.lastRewardTimestamp) {
+            uint256 lpSupply = lpToken.balanceOf(address(MCJ));
+
+            if (lpSupply > 0) {
+                uint256 timeElapsed = block.timestamp.sub(pool.lastRewardTimestamp);
+                uint256 tokenReward = timeElapsed.mul(tokenPerSec);
+                pool.accTokenPerShare = pool.accTokenPerShare.add((tokenReward.mul(ACC_TOKEN_PRECISION) / lpSupply));
+            }
+
+            pool.lastRewardTimestamp = block.timestamp;
+            poolInfo = pool;
+        }
+    }
+
     /// @notice In case rewarder is stopped before emissions finished, this function allows
     /// withdrawal of remaining tokens.
     function emergencyWithdraw() public onlyOwner {
@@ -245,16 +254,4 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
             rewardToken.safeTransfer(address(msg.sender), rewardToken.balanceOf(address(this)));
         }
     }
-
-    /// @notice View function to see balance of reward token.
-    function balance() external view returns (uint256) {
-        if (isNative) {
-            return address(this).balance;
-        } else {
-            return rewardToken.balanceOf(address(this));
-        }
-    }
-
-    /// @notice payable function needed to receive AVAX
-    receive() external payable {}
 }
